ac440a62289fc7b0a0f8fecf21d715c4e036f742
innocentdavid <paulinnocent04@gmail.com>
2023-10-12 21:45:29 +0100

    sd

--- src/dashboard/adminLogin.js
+++ src/dashboard/adminLogin.js
@@ -56,7 +56,7 @@ export default function AdminLogin() {
         <div className="flex flex-col justify-center items-center">
             <div className="shadow-2-strong p-5">
                 <div className="flex flex-col justify-center items-center pb-10">
-                    <img className="w-48 h-40 mt-10 lg:mt-14" src={sproutyLogo} alt="sprouty social" />
+                    {/* <img className="w-48 h-40 mt-10 lg:mt-14" src={sproutyLogo} alt="sprouty social" /> */}
                     <h5 className="font-bold text-[40px] text-gray20">Login to dashboard</h5>
                 </div>
                 <form action="" className="flex flex-col items-center justify-start">
@@ -88,7 +88,7 @@ export default function AdminLogin() {
                         className="bg-primaryblue rounded-[10px] py-2 px-6 text-slate-50 font-semibold text-base mb-1"
                         onClick={() => handleLogin()}
                     >
-                        Sign In Now
+                        Sign In
                     </button>
                 </form>
             </div>
